e46ed9c4cd2fd0946b02e8e512f408d08bf39cff
WemaGoodness <goodnesswema20@gmail.com>
2024-07-31 02:48:31 +0300

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,11 +4,11 @@ def canUnlockAll(boxes):
     """Determines if all the boxes can be opened."""
     if not boxes:
         return False
-    
+
     n = len(boxes)
     opened_boxes = set()
     keys = set([0])
-    
+
     while keys:
         current_key = keys.pop()
         if current_key not in opened_boxes:
@@ -16,5 +16,5 @@ def canUnlockAll(boxes):
             for key in boxes[current_key]:
                 if key < n:
                     keys.add(key)
-    
+
     return len(opened_boxes) == n
